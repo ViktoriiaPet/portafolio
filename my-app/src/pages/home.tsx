ab8e2b345ab3c8a1822bb4be90c2022de2cd2029
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-05-05 17:18:19 +0200

    feat: add little things to root pages

--- my-app/src/pages/home.tsx
+++ my-app/src/pages/home.tsx
@@ -1,8 +1,11 @@
 import { MainHeader } from "../components/headMain"
 export function Home () {
     return(
-        <>
+        <div >
         <MainHeader />
-        </>
+        <div className="bg-[#EEEAF8]">
+            <p>This is a page </p>
+        </div>
+        </div>
     )
 }
