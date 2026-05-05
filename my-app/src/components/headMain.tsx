cc4ba34b82eb4880022172f79222bfcb576d8437
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-05-05 17:18:49 +0200

    feat: create primary structure in head

--- my-app/src/components/headMain.tsx
+++ my-app/src/components/headMain.tsx
@@ -1,12 +1,25 @@
+    import { ButtonPrimary } from "./Button-primary"
     export function MainHeader() {
+
+
         return(
-            <>
-            
-            <div className = {"h-1/3 bg-blue-300"}>
-                <p>
-                    main
+            <div className="grid grid-cols-2 gap-1">
+            <div className="bg-transparent p-4 pb-6 col-span-1">
+            <div>
+                <p className="font-istok font-medium text-5xl text-neutral-600">
+                    I build. I ship. I make it <span className="text-blue-300">scale</span>.
+                </p>
+                <p className="font-istok font-medium text-sm text-neutral-400">
+                    I create modern application with clean code, great perfomance, and influence experience.
                 </p>
             </div>
-            </>
+            <div className="p-4 grid grid-cols-2 gap-4 justify-items-start">
+                <ButtonPrimary to="/work" children = "View my work"/>
+                <ButtonPrimary to="/new" children = "Let's connect" />
+            </div>
+            </div>
+
+
+            </div>
         )
     }
