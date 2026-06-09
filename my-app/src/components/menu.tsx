0a7d22c6c9d3ad1a0034620eb2229f7953ffe4b8
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-06-09 12:31:32 +0200

    fix: update logo: change image

--- my-app/src/components/menu.tsx
+++ my-app/src/components/menu.tsx
@@ -5,7 +5,7 @@ export function MenuPrimary () {
     return (
         <>
         <div className="flex flex-row items-center mt-3 text-neutral-700">
-            <button className="p-5 ml-5 w-12 h-12 bg-[url('assets/BrickWallPattern.png')] bg-contain bg-no-repeat bg-center shrink-0" onClick={() => navigate("/")} />
+            <button className="p-5 ml-5 w-12 h-12 bg-[url('assets/wallBlock.png')] bg-contain bg-no-repeat bg-center shrink-0" onClick={() => navigate("/")} />
 
             <div className="flex gap-16  mx-auto">
             <button onClick={() => navigate("/work")}> Work </button>
