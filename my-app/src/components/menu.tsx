fd0137ab75ce3cccbcc18de440b526d829e09b4b
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-06-10 16:03:04 +0200

    feat: add links to linkedIn

--- my-app/src/components/menu.tsx
+++ my-app/src/components/menu.tsx
@@ -4,17 +4,18 @@ export function MenuPrimary () {
     const navigate = useNavigate();
     return (
         <>
-        <div className="flex flex-row items-center mt-3 text-neutral-700">
+        <div className="flex flex-row items-center mt-3 mr-6 text-neutral-700 ">
             <button className="p-5 ml-5 w-12 h-12 bg-[url('assets/wallBlock.png')] bg-contain bg-no-repeat bg-center shrink-0" onClick={() => navigate("/")} />
 
-            <div className="flex gap-16  mx-auto">
-            <button onClick={() => navigate("/work")}> Work </button>
+            <div className="flex gap-16  mx-auto flex-end">
+            {/*<button onClick={() => navigate("/work")}> Work </button>
             <button onClick={() => navigate("/about")}> About </button>
             <button onClick={() => navigate("/stack")}> Stack </button>
             <button onClick={() => navigate("/blog")}> Blog </button>
-            <button> ... </button>
-            <ButtonPrimary children = "Contact me" />
+            <button> ... </button>*/}
+            
             </div>
+            <a href="https://www.linkedin.com/in/victoria-petukhova-767642348"><ButtonPrimary children = "Contact me in LinkedIn" /></a> 
         </div>
         </>
     )
